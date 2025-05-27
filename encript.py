0b1c21d978d71d3f00c31501c5b622407a27cdc6
jeromehage <jeromehage>
2025-05-27 23:05:22 +0200

    cleanup and better test

--- encript.py
+++ encript.py
@@ -1,29 +1,21 @@
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
-import math
-import os
-import argparse
-import getpass
-import hashlib
+import os, argparse
+from math import ceil
+from getpass import getpass
+from hashlib import blake2b
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
-
-def argsort(arr):
-    return sorted(range(len(arr)), key = arr.__getitem__)
 
 class chunker:
     # note: the filesplit library ain't good cz Split.bysize goes by
     # size on disk and not absolute size
-    _delim = '.c'
-
     def __init__(self, path, chunk_sz):
         # prep
-        chunk_cnt = int(math.ceil(os.path.getsize(path) / chunk_sz))
-        chunk_digits = int(math.floor(math.log10(chunk_cnt))) + 1
+        chunk_cnt = int(ceil(os.path.getsize(path) / chunk_sz))
         # save to attributes
         self.path = path
         self.chunk_sz = chunk_sz
         self.chunk_cnt = chunk_cnt # number of chunks
-        self.chunk_digits = chunk_digits # how many digits the chunk count is
 
     @property
     def chunks(self):
@@ -31,46 +23,12 @@ class chunker:
            for i in range(self.chunk_cnt):
                yield f.read(self.chunk_sz)
 
-    def split(self, output_dir, op = None):
-        fname = os.path.basename(self.path)
-        for i, chunk in enumerate(self.chunks):
-            n = '{}{}{:0{}}'.format(fname, self._delim, i, self.chunk_digits)
-            ofname = os.path.join(output_dir, n)
-            with open(ofname, mode = 'wb') as f:
-                if callable(op):
-                    f.write(op(chunk))
-                else:
-                    f.write(chunk)
-            #print(i + 1, '/', self.chunk_cnt, ofname)
-
-    @staticmethod
-    def merge(input_dir, output_path, op = None, delim = _delim):
-        parts = []
-        part_nb = []
-        for fn in os.listdir(input_dir):
-            if delim in fn:
-                pn = fn.split(delim)[-1]
-                if pn.isdigit():
-                    parts += [fn]
-                    part_nb += [int(pn)]
-        parts = [parts[i] for i in argsort(part_nb)]
-        N = len(parts)
-        with open(output_path, 'wb') as of:
-            for i, p in enumerate(parts):
-                path = os.path.join(input_dir, p)
-                #print(i + 1, '/', N, path)
-                with open(path, 'rb') as f:
-                    chunk = f.read()
-                if callable(op):
-                    of.write(op(chunk))
-                else:
-                    of.write(chunk)
-
     def do(self, op, output_path):
-        # split, do, merge (in memory)
+        # split, do (in memory), merge
         with open(output_path, 'wb') as f:
             for i, chunk in enumerate(self.chunks):
                 f.write(op(chunk))
+                #print(i + 1, '/', self.chunk_cnt)
 
 def chunk_encrypt(data, password, tag = b''):
     # convergence encryption
@@ -80,15 +38,15 @@ def chunk_encrypt(data, password, tag = b''):
     # first make a salt from the password
     # salt is used to add consistent randomness to the encryption key
     # that way confirmation attacks are difficult
-    salt = hashlib.blake2b(password_bytes, digest_size = 16).digest()
+    salt = blake2b(password_bytes, digest_size = 16).digest()
     # calculate the hash of the data, which is the key
-    key = hashlib.blake2b(data, digest_size = 32, salt = salt).digest()
+    key = blake2b(data, digest_size = 32, salt = salt).digest()
     # encrypt the data
     nonce = b'\x00' * 12
     chacha = ChaCha20Poly1305(key)
     enc_data = chacha.encrypt(nonce, data, tag)
     # encrypt the key with the password also
-    pw = hashlib.blake2b(password_bytes, digest_size = 32).digest()
+    pw = blake2b(password_bytes, digest_size = 32).digest()
     chacha = ChaCha20Poly1305(pw)
     enc_key = chacha.encrypt(nonce, key, tag)
     # stick them together
@@ -100,7 +58,7 @@ def chunk_decrypt(enc_data, password, tag = b''):
     # decrypt key
     enc_key = enc_data[-48:]
     nonce = b'\x00' * 12
-    pw = hashlib.blake2b(password_bytes, digest_size = 32).digest()
+    pw = blake2b(password_bytes, digest_size = 32).digest()
     chacha = ChaCha20Poly1305(pw)
     key = chacha.decrypt(nonce, enc_key, tag)
     # decrypt data
@@ -108,17 +66,15 @@ def chunk_decrypt(enc_data, password, tag = b''):
     data = chacha.decrypt(nonce, enc_data[:-48], tag)
     return data
 
-##
-
 def encrypt(path, password, chunk_sz):
-    io = chunker(path, chunk_sz)
+    chunk = chunker(path, chunk_sz)
     encryptor = lambda data: chunk_encrypt(data, password)
-    io.do(encryptor, path + '.enc')
+    chunk.do(encryptor, path + '.enc')
 
 def decrypt(path, password, chunk_sz):
-    io = chunker(path, chunk_sz + 64)
+    chunk = chunker(path, chunk_sz + 64)
     decryptor = lambda data: chunk_decrypt(data, password)
-    io.do(decryptor, path + '.dec')
+    chunk.do(decryptor, path + '.dec')
 
 if __name__ == '__main__':
 
@@ -136,7 +92,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--chunksize', default = _128_MB)
     args = parser.parse_args()
 
-    pw = getpass.getpass('password:')
+    pw = getpass('password:')
     if args.encrypt:
         encrypt(args.path, pw, args.chunksize)
     if args.decrypt:
--- test_encript.py
+++ test_encript.py
@@ -1,92 +1,49 @@
 if __name__ == '__main__':
 
     from encript import *
-    import os, shutil
+    import os
     import hashlib
 
-    def print_hash(path):
+    def hash_file(path):
         checksum = hashlib.sha1()
         with open(path, 'rb') as f:
             checksum.update(f.read())
-        h = checksum.hexdigest()
-        print(path, h)
-        return h
+        return checksum.hexdigest()
 
-    def prep_folder(path):
-        # create temp folder
-        if not os.path.exists(path):
-            os.mkdir(path)
-        # clear stuff there
-        for f in os.listdir(path):
-            fp = os.path.join(path, f)
-            os.remove(fp)
+    def hash_bytes(b):
+        checksum = hashlib.sha1()
+        checksum.update(b)
+        return checksum.hexdigest()
 
-    def encrypt_totemp(path, password, chunk_sz):
-        prep_folder('temp')
-        c = chunker(path, chunk_sz)
-        encryptor = lambda data: chunk_encrypt(data, password)
-        c.split('temp', encryptor)
-        chunker.merge('temp', path + '.enc')
-
-    def decrypt_totemp(path, password, chunk_sz):
-        prep_folder('temp')
-        c = chunker(path, chunk_sz + 64)
-        c.split('temp')
-        decryptor = lambda data: chunk_decrypt(data, password)
-        chunker.merge('temp', path + '.dec', decryptor)
-
-    # start by making two test files with duplicate parts
-    input_file = 'Win10_20H2_v2_EnglishInternational_x64.iso'
-
-    _MB = pow(2, 20)
-    _128_MB = 128 * _MB
-
-    # 1GB file
-    prep_folder('data')
-    c = chunker(input_file, 1024 * _MB)
-    c.split('data')
-    file = [f for f in os.listdir('data') if f.endswith('.c0')][0]
-    shutil.copy(os.path.join('data', file), 'testfile.db')
-    # 1GB and a bit more
-    c = chunker(input_file, 1200 * _MB)
-    c.split('data')
-    file = [f for f in os.listdir('data') if f.endswith('.c0')][0]
-    shutil.copy(os.path.join('data', file), 'testfile2.db')
-
-    # encrypt and decrypt to temp folder
+    chunk_sz = 32 * pow(2, 20)
     password = 'shawarma_djej'
 
-    # first test file
-    encrypt_totemp('testfile.db', password, _128_MB)
-    file = os.path.join('temp', os.listdir('temp')[0])
-    h1 = print_hash(file)
-    decrypt_totemp('testfile.db.enc', password, _128_MB)
-    file = os.path.join('temp', os.listdir('temp')[0])
-    h2 = print_hash(file)
-    # the other file
-    encrypt_totemp('testfile2.db', password, _128_MB)
-    file = os.path.join('temp', os.listdir('temp')[0])
-    h3 = print_hash(file)
-    decrypt_totemp('testfile2.db.enc', password, _128_MB)
-    file = os.path.join('temp', os.listdir('temp')[0])
-    h4 = print_hash(file)
-    assert(h1 == h2 == h3 == h4)
+    encrypt('testfile.db', password, chunk_sz)
+    decrypt('testfile.db.enc', password, chunk_sz)
+    encrypt('testfile2.db', password, chunk_sz)
+    decrypt('testfile2.db.enc', password, chunk_sz)
 
-    # encrypt and decrypt directly
-    encrypt('testfile.db.enc.dec', password, 3 * _128_MB)
-    decrypt('testfile.db.enc.dec.enc', password, 3 * _128_MB)
-    encrypt('testfile2.db.enc.dec', password, 3 * _128_MB)
-    decrypt('testfile2.db.enc.dec.enc', password, 3 * _128_MB)
+    # compare final hashes
+    h1 = hash_file('testfile.db')
+    h2 = hash_file('testfile.db.enc.dec')
+    h3 = hash_file('testfile2.db')
+    h4 = hash_file('testfile2.db.enc.dec')
+    assert(h1 == h2)
+    assert(h3 == h4)
 
-    # compare hashes
-    h1 = print_hash('testfile.db')
-    h2 = print_hash('testfile.db.enc.dec')
-    h3 = print_hash('testfile.db.enc.dec.enc.dec')
-    h4 = print_hash('testfile2.db')
-    h5 = print_hash('testfile2.db.enc.dec')
-    h6 = print_hash('testfile2.db.enc.dec.enc.dec')
-
-    assert(h1 == h2 == h3)
-    assert(h4 == h5 == h6)
+    # compare encrypted chunk hashes
+    t1 = chunker('testfile.db.enc', chunk_sz + 64)
+    t2 = chunker('testfile2.db.enc', chunk_sz + 64)
+    t3 = chunker('testfile.db', chunk_sz)
+    t4 = chunker('testfile2.db', chunk_sz)
+    for c1, c2, c3, c4 in zip(t1.chunks, t2.chunks, t3.chunks, t4.chunks):
+        h1 = hash_bytes(c1)
+        h2 = hash_bytes(c2)
+        assert(h1 == h2)
+        h3 = hash_bytes(c3)
+        h4 = hash_bytes(c4)
+        h5 = hash_bytes(chunk_decrypt(c1, password))
+        h6 = hash_bytes(chunk_decrypt(c2, password))
+        assert(h3 == h4 == h5 == h6)
 
     print('Success!')
